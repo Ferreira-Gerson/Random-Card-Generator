08e092398f06ffe4dbff48073d7b9189b78be8db
Ferreira-Gerson <gersonferreira90@gmail.com>
2022-09-22 08:26:21 +0000

    First commit need to do more changings

--- src/app.js
+++ src/app.js
@@ -4,8 +4,34 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+let generateRandomNumber = () => {
+  let cards = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indxCards = Math.floor(Math.random() * cards.length);
+  return cards[indxCards];
+};
 
-window.onload = function() {
+let generateRandomSuit = () => {
+  let naipe = ["diamond", "spade", "heart", "cub"];
+  let indxNaipe = Math.floor(Math.random() * naipe.length);
+  return naipe[indxNaipe];
+};
+
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML = generateRandomNumber();
 };
